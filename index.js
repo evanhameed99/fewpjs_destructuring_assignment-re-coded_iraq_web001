37642bb70fbb63868b0508c2ba899ac1def1aca1
Evan hameed <evanhameed9966@gmail.com>
2020-05-25 11:43:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ const k_muppet = {
   console.log(meow,haahee,meeeah,khaa,qeaqeq);
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
+const [Bessie, ,Dolly,Babe,Little] ='cow horse sheep pig chicken'.split(' ');
+console.log(Bessie,Dolly,Babe,Little);
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
 
